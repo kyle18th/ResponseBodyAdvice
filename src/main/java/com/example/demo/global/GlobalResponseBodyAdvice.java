e4e6310480ea51a70b907825b19697c01e4f5c98
Kyle <tk111404207@gmail.com>
2018-12-19 11:58:09 +0800

    Add annotations

--- src/main/java/com/example/demo/global/GlobalResponseBodyAdvice.java
+++ src/main/java/com/example/demo/global/GlobalResponseBodyAdvice.java
@@ -9,9 +9,10 @@ import org.springframework.http.MediaType;
 import org.springframework.http.server.ServerHttpRequest;
 import org.springframework.http.server.ServerHttpResponse;
 import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseBodyAdvice;
 
-@ControllerAdvice
+@ControllerAdvice(annotations = {RestController.class})
 public class GlobalResponseBodyAdvice implements ResponseBodyAdvice {
 
     private final ObjectMapper objectMapper;
